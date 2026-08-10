269f2df81f96a7d60b2642f3088a53628befb169
Nukala-Sandeep <nukalasandeepnsp143@gmail.com>
2026-08-10 21:05:18 +0530

    Initial commit

---
+++ Student.java
@@ -0,0 +1,11 @@
+package com.javaintro;
+
+public class Student {
+
+	public static void main(String[] args) {
+		System.out.println("Sadeep");
+		System.out.println("Venky");
+		System.out.println("Rukmini");
+	}
+
+}
