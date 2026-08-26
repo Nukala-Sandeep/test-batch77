82943ff1129b59d1b2ee9c11dddd3f3a3f3f4eec
Nukala-Sandeep <nukalasandeepnsp143@gmail.com>
2026-08-26 16:11:00 +0530

    new commit19

---
+++ javafumdamentals/Assignment25_8.java
@@ -0,0 +1,17 @@
+package com.javafumdamentals;
+
+public class Assignment25_8 {
+	
+	
+	public static void main(String[] args) {
+		int a=10;
+		Integer i=a;
+		int b=i;
+		
+		System.out.println(a);
+		System.out.println(++i);
+		System.out.println(++b);
+	}
+	
+
+}
